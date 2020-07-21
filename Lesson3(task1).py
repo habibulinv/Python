50958844a7abbe2182064a768d4142a06ce9e53a
Vladimir Khabibulin <67709112+habibulinv@users.noreply.github.com>
2020-07-21 21:31:15 +0300

    Update Lesson3(task1).py
    Внес исправление. Добавил try except

--- Lesson3(task1).py
+++ Lesson3(task1).py
@@ -6,8 +6,17 @@
 """
 
 
-def func(a=int(input("Введите делимое A: ")), b=int(input("Введите делитель B: "))):
-    return (a / b)
+def func(a=input("Введите делимое A: "), b=input("Введите делитель B: ")):
+    try:
+        a = int(a)
+        b = int(b)
+    except ValueError:
+        print("В параметры должнs быть внесены числа")
+    try:
+        return (a / b)
+    except ZeroDivisionError:
+        return ("на ноль делить нельзя")
+
 
 
 print(func())
