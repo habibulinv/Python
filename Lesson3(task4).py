3e613ddd1c27900034d7cabd9c830826d7c3f55b
Vladimir Khabibulin <67709112+habibulinv@users.noreply.github.com>
2020-07-21 21:32:42 +0300

    Update Lesson3(task4).py
    Внес исправления. Переписал логику и добавил try except

--- Lesson3(task4).py
+++ Lesson3(task4).py
@@ -11,12 +11,19 @@ def my_func(x, y):
 
 
 def my_func(x, y):
-    x = abs(x)
-    y = abs(y) * (-1)
-    while y < -1:
-        x = x * x
-        y += 1
-    return 1 / x
+    try:
+        x = float(x)
+        y = int(y)
+        if x <= 0 or y >= 0:
+            return ("Вы ввели не верное значение х и y. Х - положительный, Y - отрицательный")
+        else:
+            result = 1
+            for _ in range(abs(y)):
+                result *= 1 / x
+            return f'Резултат {round(result, 6)}'
+    except ValueError:
+        return "Нужно ввести именно число"
+
 
 
 print(my_func(40, -2))
